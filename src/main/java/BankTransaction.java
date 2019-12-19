b674c1a9dbfe77512e3bdd081c09cd6a9c62d80c
MaksimDemin <mdeminalex@icloud.com>
2019-12-19 16:46:03 +0800

    changed class BankTransaction, clear class

--- src/main/java/BankTransaction.java
+++ src/main/java/BankTransaction.java
@@ -8,38 +8,23 @@ public class BankTransaction {
     private String contractor;
 
     private BigDecimal incomeAmount;
-//    private double incomeAmountDouble;
-
     private BigDecimal expenseAmount;
-//    private double expenseAmountDouble;
-
-
     private String MCCCode;
     private TypeTransaction typeTransaction;
     BigDecimal bdNull = new BigDecimal(0);
 
 
     BankTransaction(String descriptionTransaction, BigDecimal expenseAmount, BigDecimal incomeAmount, String MCCCode) {
-//      BankTransaction(String descriptionTransaction, double expenseAmountDouble, double incomeAmountDouble) {
         this.descriptionTransaction = descriptionTransaction;
-
         this.expenseAmount = expenseAmount;
-//          this.expenseAmountDouble = expenseAmountDouble;
-
         this.incomeAmount = incomeAmount;
-//          this.incomeAmountDouble = incomeAmountDouble;
 
         if (incomeAmount.compareTo(new BigDecimal("0.0")) > 0 && expenseAmount.equals(new BigDecimal("0.0"))) {
             typeTransaction = TypeTransaction.INCOME;
         }
-//        if (incomeAmountDouble > 0 && expenseAmountDouble == 0)
-//            typeTransaction = TypeTransaction.INCOME;
-
         if (expenseAmount.compareTo(new BigDecimal("0.0")) > 0 && incomeAmount.equals(new BigDecimal("0.0"))) {
             typeTransaction = TypeTransaction.EXPENSE;
         }
-//        if (expenseAmountDouble > 0 && incomeAmountDouble == 0)
-//            typeTransaction = TypeTransaction.EXPENSE;
 
         this.MCCCode = MCCCode;
     }
@@ -47,7 +32,6 @@ public class BankTransaction {
     public String getDescriptionTransaction() {
         return descriptionTransaction;
     }
-
 
     public BigDecimal getExpenseAmount() {
         return expenseAmount;
